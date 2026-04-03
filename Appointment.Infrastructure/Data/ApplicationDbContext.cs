2d9301a3da490eaa5826e6e6e2396fdba0263cbb
Choffel <daniilmiadzve@gmail.com>
2026-04-03 21:30:45 +0300

    feat: create infrastructure layer with ApplicationDbContext and repositories

---
+++ Appointment.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Appointment.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Appointment.Infrastructure.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Patient> patients { get; set; }
+}
---
+++ Appointment.Infrastructure/Repository/PatientRepository.cs
@@ -0,0 +1,46 @@
+﻿using Appointment.Application.Contracts;
+using Appointment.Domain.Models;
+using Appointment.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Appointment.Infrastructure.Repository;
+
+public class PatientRepository : IPatientRepository
+{
+    private readonly ApplicationDbContext _dbContext;
+    
+    public PatientRepository(ApplicationDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+    public async Task<Patient> GetById(Guid id)
+    {
+        // can use findAsync()
+       return await _dbContext.patients.FirstOrDefaultAsync(p => p.Id == id);
+    }
+
+    public async Task<List<Patient>> GetAll()
+    {
+        return await _dbContext.patients.ToListAsync();
+    }
+
+    public async Task<Patient> Add(Patient patient)
+    {
+        var add = await  _dbContext.patients.AddAsync(patient);
+        
+        return add.Entity;
+    }
+
+    public  async Task<Patient> Update(Patient patient)
+    {
+        var update = _dbContext.patients.Update(patient);
+        
+        return update.Entity;
+    }
+
+    
+    public async Task Delete(Patient patient)
+    {
+        _dbContext.patients.Remove(patient);
+    }
+}
