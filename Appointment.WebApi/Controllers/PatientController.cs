4c5d16112c76ad0b5b8e41adb1b73a132d785b27
Choffel <daniilmiadzve@gmail.com>
2026-04-04 03:02:00 +0300

    feat: add controllers

---
+++ Appointment.WebApi/Controllers/PatientController.cs
@@ -0,0 +1,46 @@
+﻿using Appointment.Application.Contracts;
+using Appointment.Application.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Appointment.WebApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class PatientController : ControllerBase 
+{
+    private readonly IPatientService _patientService;
+    
+    public PatientController(IPatientService patientService)
+    {
+        _patientService = patientService;
+    }
+
+    [HttpPost("Create")]
+    public async Task<IActionResult> CreateAsync([FromBody] CreatePatientDto request)
+    {
+        var result = await _patientService.Create(request);
+        return Ok(result);
+    }
+
+    [HttpPost("Update/{patientId:guid}")] 
+    public async Task<IActionResult> UpdateAsync([FromRoute] Guid patientId, [FromBody] PatientUpdateDto request)
+    {
+        var result = await _patientService.Update(patientId, request);    
+        return Ok(result);
+    }
+
+    [HttpPost("Delete/{patientId:guid}")]  
+    public async Task<IActionResult> DeleteAsync([FromRoute] Guid patientId)
+    { 
+        await _patientService.Delete(patientId);  
+        
+        return NotFound();  
+    }
+
+    [HttpGet("GetPatientById/{patientId:guid}")]  
+    public async Task<IActionResult> GetPatientByIdAsync([FromRoute] Guid patientId)
+    {
+        var result = await _patientService.GetById(patientId);  
+        return result != null ? Ok(result) : NotFound();
+    }
+}
