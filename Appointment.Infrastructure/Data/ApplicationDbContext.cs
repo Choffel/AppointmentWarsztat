0b756b7e0659efdea150f1f54ab98e0914b42e85
Choffel <daniilmiadzve@gmail.com>
2026-04-06 02:06:05 +0300

    feat: add FluentApi for Patient

--- Appointment.Infrastructure/Data/ApplicationDbContext.cs
+++ Appointment.Infrastructure/Data/ApplicationDbContext.cs
@@ -11,4 +11,20 @@ public class ApplicationDbContext : DbContext
     }
 
     public DbSet<Patient> patients { get; set; }
+    public DbSet<Domain.Models.Appointment> appointments { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Patient>(entity =>
+        {
+            entity.HasKey(e => e.Id);
+
+            entity.HasMany<Domain.Models.Appointment>(q => q.Appointments)
+                .WithOne(p => p.Patient)
+                .HasForeignKey(e => e.PatientId)
+                .OnDelete(DeleteBehavior.Cascade);
+        });
+    }
 }
