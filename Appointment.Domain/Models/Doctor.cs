132d2724b518dbae7557054f958402c75e1b2544
Choffel <daniilmiadzve@gmail.com>
2026-04-09 00:12:44 +0300

    feat: add Doctor domain model

---
+++ Appointment.Domain/Models/Doctor.cs
@@ -0,0 +1,18 @@
+﻿namespace Appointment.Domain.Models;
+
+public abstract class Doctor
+{
+    public string FirstName { get; set; }
+    
+    public string LastName { get; set; }
+    
+    public string Email { get; set; }
+    
+    public string PhoneNumber { get; set; }
+    
+    public string Address { get; set; }
+    
+    public string Sity { get; set; }
+    
+    public string Specialty { get; set; }
+}
