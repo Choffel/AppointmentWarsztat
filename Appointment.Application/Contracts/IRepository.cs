dd04eb10ede069bc51c7def3c7d0a70e5798ad0e
Choffel <daniilmiadzve@gmail.com>
2026-04-08 14:25:28 +0300

    refactor: replace specific repositories with generic IRepository<T>

---
+++ Appointment.Application/Contracts/IRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace Appointment.Application.Contracts;
+
+public interface IRepository<T> where T : class
+{
+    Task<List<T>> GetAll();
+    
+    Task<T> GetById(Guid id);
+    
+    T Add(T entity);
+    
+    T Update(T entity);
+    
+    Task Delete(Guid id);
+}
---
+++ Appointment.Infrastructure/Repository/Repository.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Appointment.Application.Contracts;
+using Appointment.Infrastructure.Data;
+
+namespace Appointment.Infrastructure.Repository;
+
+public class Repository<T> : IRepository<T> where T : class
+{
+    private readonly ApplicationDbContext _context;
+    protected DbSet<T> _dbSet;
+    
+    public Repository(ApplicationDbContext context)
+    {
+        _context = context;
+        _dbSet = context.Set<T>();
+    }
+
+
+    public Task<List<T>> GetAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<T> GetById(Guid id)
+    {
+        return await _dbSet.FindAsync(id);
+    }
+
+    public  virtual T Add(T entity)
+    {
+        return  _dbSet.Add(entity).Entity;
+    }
+
+    public virtual  T Update(T entity)
+    {
+        return _dbSet.Update(entity).Entity;
+    }
+
+    public async Task Delete(Guid id)
+    {
+        var entity = await GetById(id);
+        if(entity != null) 
+        {
+            _dbSet.Remove(entity);
+        }
+    }
+}
