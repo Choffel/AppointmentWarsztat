6a80053b3c5092872a6d2a28a4cd0b39d2925c5a
Choffel <daniilmiadzve@gmail.com>
2026-04-09 00:13:51 +0300

    feat:add Appointment mapper contract and implementation

---
+++ Appointment.Application/Contracts/IAppointmentMapper.cs
@@ -0,0 +1,9 @@
+﻿using Appointment.Application.DTOs;
+
+namespace Appointment.Application.Contracts;
+
+public interface IAppointmentMapper
+{
+    AppointmentResponseDto ToResponseDto(Domain.Models.Appointment appointment);
+    // List<AppointmentResponseDto> ToResponseDtos(List<Appointment> appointments);
+}
---
+++ Appointment.Application/Mappers/AppointmentMapper.cs
@@ -0,0 +1,23 @@
+﻿using Appointment.Application.Contracts;
+using Appointment.Application.DTOs;
+
+namespace Appointment.Application.Mappers;
+
+public class AppointmentMapper  : IAppointmentMapper
+{
+    public  AppointmentResponseDto ToResponseDto(Domain.Models.Appointment appointment)
+    {
+        return new AppointmentResponseDto(
+            appointment.Id,
+            appointment.Patient?.Name ?? "Unknown", // Безопасное обращение
+            appointment.PatientId,
+            appointment.Id, // Пока нет DoctorId — используем Id (заменить потом)
+            "Dr. Smith", // TODO: добавить Doctor в модель
+            appointment.Date,
+            appointment.StartTime,
+            appointment.EndTime,
+            appointment.Status.ToString(),
+            appointment.Description ?? ""
+        );
+    }
+}
