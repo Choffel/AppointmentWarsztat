0aa02609d164a0deecb5b3ae7245c6da3535d765
Choffel <daniilmiadzve@gmail.com>
2026-04-09 18:45:33 +0300

    feat: add AppointmentChecker for schedule validation

---
+++ Appointment.Infrastructure/AppointmentChecker.cs
@@ -0,0 +1,31 @@
+﻿using System.Data.Entity;
+using Appointment.Application.Contracts;
+
+namespace Appointment.Infrastructure;
+
+public class AppointmentChecker : IAppointmentChecker
+{
+    
+    private readonly IRepository<Domain.Models.Appointment> _appointmentRepository;
+    
+    public AppointmentChecker(IRepository<Domain.Models.Appointment> appointmentRepository)
+    {
+        _appointmentRepository = appointmentRepository;
+    }
+    
+    public async Task<bool> HasOverlappingAppointmentAsync(Guid doctorId, Guid appointmentId, DateOnly date, TimeOnly end, TimeOnly start)
+    {
+        var overlapping = await _appointmentRepository
+            .GetQueryable()  
+            .Where(a => a.DoctorId == doctorId &&
+                        a.Id != appointmentId &&  
+                        a.Date == date &&
+                        (a.StartTime < end && a.EndTime > start))
+            .FirstOrDefaultAsync();
+
+        if (overlapping != null)
+            throw new InvalidOperationException("Doctor is busy");
+        
+        return true;
+    }
+}
