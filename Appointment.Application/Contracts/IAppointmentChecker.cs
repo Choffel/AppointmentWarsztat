142a8a93eb13c05513233399b86616aa9beb3f52
Choffel <daniilmiadzve@gmail.com>
2026-04-09 18:45:32 +0300

    feat: add IAppointmentService and IAppointmentChecker contracts

---
+++ Appointment.Application/Contracts/IAppointmentChecker.cs
@@ -0,0 +1,6 @@
+﻿namespace Appointment.Application.Contracts;
+
+public interface IAppointmentChecker
+{
+    Task<bool> HasOverlappingAppointmentAsync(Guid doctorId, Guid appointmentId, DateOnly date, TimeOnly start, TimeOnly end);
+}
---
+++ Appointment.Application/Contracts/IAppointmentService.cs
@@ -0,0 +1,14 @@
+﻿using Appointment.Application.DTOs;
+
+namespace Appointment.Application.Contracts;
+
+public interface IAppointmentService
+{
+    Task<AppointmentResponseDto> CreateAppointment(CreateAppointmentDto dto);
+    
+    Task<AppointmentResponseDto> GetAppointmentById(Guid patientId, Guid appointmentId);
+    
+     // Task<AppointmentResponseDto> ChangeAppointmentStatus(Guid patientId, Guid appointmentId, DateOnly date);
+    
+    Task<AppointmentResponseDto> DeleteAppointment(Guid patientId, Guid appointmentId);
+}
