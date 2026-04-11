60169204c597bcdf35747d72e860a4ddbce0c3b5
Choffel <daniilmiadzve@gmail.com>
2026-04-12 01:35:12 +0300

    feat(application): implement doctor and patient mappers

---
+++ Appointment.Application/Mappers/DoctorMapper.cs
@@ -0,0 +1,22 @@
+﻿using Appointment.Application.Contracts;
+using Appointment.Application.DTOs.DoctorDTos;
+using Appointment.Domain.Models;
+
+namespace Appointment.Application.Mappers;
+
+public class DoctorMapper : IDoctorMapper
+{
+    public ResponseDoctorDto ToResponse(Doctor doctor)
+    {
+        return new ResponseDoctorDto(
+            doctor.DoctorId,
+            doctor.FirstName,
+            doctor.LastName,
+            doctor.Email,
+            doctor.PhoneNumber,
+            doctor.Specialty,
+            doctor.Address,
+            doctor.Sity); 
+    }
+    
+}
---
+++ Appointment.Application/Mappers/PatientMapper.cs
@@ -0,0 +1,26 @@
+﻿using Appointment.Application.Contracts;
+using Appointment.Application.DTOs;
+using Appointment.Domain.Models;
+
+namespace Appointment.Application.Mappers;
+
+public class PatientMapper : IPatientMapper
+{
+    public PatientResponse ToResponse(Patient patient)
+    {
+        ArgumentNullException.ThrowIfNull(patient);
+
+        return new PatientResponse(
+            patient.Id,
+            patient.FirstName,
+            patient.LastName,
+            patient.Email,
+            patient.PhoneNumber,
+            patient.Gender,
+            patient.DateOfBirth,
+            patient.Address,
+            patient.City,
+            patient.Pesel
+        );
+    }
+}
