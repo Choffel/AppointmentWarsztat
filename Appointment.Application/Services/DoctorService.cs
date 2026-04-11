5d3fc018d7b71357a80b6188a7f63bf1afea36d0
Choffel <daniilmiadzve@gmail.com>
2026-04-12 01:35:18 +0300

    feat(application): implement DoctorService CRUD operations

---
+++ Appointment.Application/Services/DoctorService.cs
@@ -0,0 +1,20 @@
+﻿using Appointment.Application.Contracts;
+using Appointment.Application.DTOs.DoctorDTos;
+
+namespace Appointment.Application.Services;
+
+public class DoctorService : IDoctorService
+{
+    private readonly IRepository<Domain.Models.Doctor> _doctorRepository;
+    private readonly IDoctorMapper _mapper;
+    private readonly IUnitOfWork _unitOfWork;
+    
+    
+    public async Task<Domain.Models.Doctor> CreateDoctor(CreateDoctorDto dto)
+    {
+        var doctor = _mapper.ToEntity(dto);
+        _doctorRepository.Add(doctor);
+        await _unitOfWork.CommitAsync();
+        return doctor;
+    }
+}
